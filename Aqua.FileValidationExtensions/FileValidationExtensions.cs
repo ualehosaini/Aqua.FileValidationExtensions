51ae5dd1557fa2e6f8a473ea9eb8a333ad5d8d94
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-11-14 21:19:24 +0000

    IsValidFile has been added

---
+++ Aqua.FileValidationExtensions/FileValidationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Aqua.FileValidationExtensions
+{
+    public static class FileValidationExtensions
+    {
+        /// <summary>
+        /// Validate the input string is not Empty
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static bool IsNullOrEmptyOrWhiteSpace(this string input) => string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input);
+
+        /// <summary>
+        /// Validate the file is exists
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static bool IsValidFile(this string fileFullPath) => fileFullPath.IsNullOrEmptyOrWhiteSpace() && File.Exists(fileFullPath);
+
+
+
+
+    }
+}
